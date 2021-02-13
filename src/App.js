7c9b422d7409339a828978eef57b83ef7382fc84
Deniz Ozkan <ibrahimdenizozkan@gmail.com>
2021-02-13 11:46:00 +0300

    Add library component

--- src/App.js
+++ src/App.js
@@ -1,25 +1,31 @@
 import React, { useState } from "react";
 
 // Import components
-import Player from './components/Player'
-import Song from './components/Song'
+import Player from "./components/Player";
+import Song from "./components/Song";
+import Library from "./components/Library";
 
 // Import styles
-import style from './styles/app.scss'
+import style from "./styles/app.scss";
 
 // Import utils
-import data from './util'
+import data from "./util";
 
 function App() {
   // States
-  const [songs, setSongs] = useState(data())
-  const [currentSong, setCurrentSong] = useState(songs[0])
-  const [isPlaying, setIsPlaying] = useState(false)
+  const [songs, setSongs] = useState(data());
+  const [currentSong, setCurrentSong] = useState(songs[0]);
+  const [isPlaying, setIsPlaying] = useState(false);
 
   return (
     <div className="App">
       <Song currentSong={currentSong} />
-      <Player isPlaying={isPlaying} setIsPlaying={setIsPlaying} currentSong={currentSong} />
+      <Player
+        isPlaying={isPlaying}
+        setIsPlaying={setIsPlaying}
+        currentSong={currentSong}
+      />
+      <Library songs={songs} />
     </div>
   );
 }
