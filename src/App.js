694d8e8535417de798d6459b11c88daa4bd41dab
Deniz Ozkan <ibrahimdenizozkan@gmail.com>
2021-03-11 23:57:55 +0300

    Fix responsive issues

--- src/App.js
+++ src/App.js
@@ -47,7 +47,7 @@ function App() {
   };
 
   return (
-    <div className="App">
+    <div className={`App ${libraryStatus ? "library-active" : ""}`}>
       <Nav libraryStatus={libraryStatus} setlibraryStatus={setlibraryStatus} />
       <Song currentSong={currentSong} />
       <Player
