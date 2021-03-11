a9fa2b61118f0caf0502cb5539eb3c9520821e0e
Deniz Ozkan <ibrahimdenizozkan@gmail.com>
2021-03-11 19:28:04 +0300

    Calculate loaded bar as percentage

--- src/App.js
+++ src/App.js
@@ -19,7 +19,8 @@ function App() {
   const [isPlaying, setIsPlaying] = useState(false);
   const [songInfo, setSongInfo] = useState({
     current: 0,
-    duration: 0
+    duration: 0,
+    animationPercentage: 0
   });
   const [libraryStatus, setlibraryStatus] = useState(false);
 
@@ -30,8 +31,13 @@ function App() {
   const timeUpdateHandler = (e) => {
     const current = e.target.currentTime;
     const duration = e.target.duration;
+    const roundedCurrent = Math.round(current);
+    const roundedDuration = Math.round(duration);
+    const animationPercentage = Math.round(
+      (roundedCurrent / roundedDuration) * 100
+    );
 
-    setSongInfo({ ...songInfo, current, duration });
+    setSongInfo({ ...songInfo, current, duration, animationPercentage });
   };
 
   return (
